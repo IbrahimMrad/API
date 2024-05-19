4d717ee48e17e3856c361981ddab19f191c8d1b1
admin <ibmrad60@gmail.com>
2024-05-19 20:26:38 +0300

    Adding a new endpoint called Students

--- api.js
+++ api.js
@@ -19,8 +19,13 @@ app.use(cors());
 
 // your code
 app.get('/', function (req, res) {
-    res.send('Hello World')
-  });
+  res.send('Hello World')
+});
+
+app.get('/students', function (req, res) {
+  let data = [{id:54,name:'Sophie'},{id:17,name:'Hussein'},{id:128,name:'Fatima'}]
+  res.send(data)
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
